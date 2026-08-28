095cef60cdfe5e7ddb0335d5cc578c8fcba5a805
Alicite <lunacarter1503@gmail.com>
2026-08-27 21:36:39 -0300

    feat: Cria função de busca a API de tradução

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ const fetchTrivia = async (endpoint='', qtd=5) => {
         console.error(erro.message)
     }
 }
+
+const fetchTradutor = async (texto) => {
+    try {
+        const url = `https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=pt-BR&q=${encodeURIComponent(texto)}`;
+        let resultado = await fetch(url);
+        resultado = await resultado.json();
+
+        return resultado[0][0];
+    } catch (e) {
+        console.error(e.message);
+    }
+}
